9b6b82fe9b7dd3a87970119231902135b90a4396
ndgkhanhs <ndgkhanhsss@gmail.com>
2025-06-01 22:01:51 +0700

    number 28

---
+++ 28_Max_Of_Three.py
@@ -0,0 +1,11 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+def max_of_three(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
+print("The maximum of the three numbers is:", max_of_three(a, b, c))
