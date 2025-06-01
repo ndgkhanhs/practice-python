63b1a95c722bd819137f520d51f669646b79ba83
ndgkhanhs <ndgkhanhsss@gmail.com>
2025-06-01 22:05:43 +0700

    no 28 function

--- 28_Max_Of_Three.py
+++ 28_Max_Of_Three.py
@@ -1,6 +1,9 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-c = int(input("Enter third number: "))
+def input_numbers():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    c = int(input("Enter third number: "))
+    return a, b, c
+
 def max_of_three(a, b, c):
     if a >= b and a >= c:
         return a
@@ -8,4 +11,7 @@ def max_of_three(a, b, c):
         return b
     else:
         return c
-print("The maximum of the three numbers is:", max_of_three(a, b, c))
+    
+if __name__ == "__main__":
+    a, b, c = input_numbers()
+    print("The maximum of the three numbers is:", max_of_three(a, b, c))
